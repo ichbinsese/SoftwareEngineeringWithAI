7c1b336d950e2493fb867f44b3e889c271016843
Sebastian <76916629+ichbinsese@users.noreply.github.com>
2026-05-26 13:59:32 +0200

    Finalized Design Phase Templates

---
+++ Code/Scripts/implement.py
@@ -0,0 +1,4 @@
+from Code.Common.ai_agent import AgentUtils
+from Code.Common.prompt_utils import PromptUtils
+from Code.Common.iteration_manager import IterationManager
+from Code.Common.project_utils import ProjectUtils
---
+++ Implementation/Waveshare/battery.py
@@ -0,0 +1,28 @@
+class Battery:
+    """Battery management class"""
+
+    def __init__(self):
+        """Initialize battery management"""
+        from Implementation.Waveshare.MicroPython.waveshare_battery import init
+
+        init()
+
+    @property
+    def percentage(self):
+        """Get the current battery percentage"""
+        from Implementation.Waveshare.MicroPython.waveshare_battery  import get_percentage
+
+        return get_percentage()
+
+    @property
+    def voltage(self):
+        """Get the current battery voltage in millivolts"""
+        from Implementation.Waveshare.MicroPython.waveshare_battery  import get_voltage
+
+        return get_voltage()
+
+    def read(self):
+        """read raw battery ADC value"""
+        from Implementation.Waveshare.MicroPython.waveshare_battery  import read
+
+        return read()
